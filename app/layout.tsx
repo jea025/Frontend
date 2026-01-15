68c84faf0ad9caf0c413c17e262b8e676e8c0fad
Nicolás Monzón <nmonzon.2022@gmail.com>
2026-01-14 23:00:12 -0300

    Se habilitan las analíticas de Vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from "@/components/Footer/Footer";
+import { Analytics } from "@vercel/analytics/next";
 
 
 const geistSans = localFont({
@@ -40,6 +41,7 @@ export default function RootLayout({
         <Navbar/>
         {children}
         <Footer/>
+        <Analytics />
       </body>
     </html>
   );
